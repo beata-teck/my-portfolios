aea524d256cb56a1cc577805cb59debdb3b446e7
beata-teck <batatefera7@gmail.com>
2025-10-12 17:18:33 -0800

    Refactor App component to include AboutSection, ContactForm, Projects, and NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import Contact  from "./Pages/Contact";
 import About from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import NotFound from "./Pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProjectCard from "./components/ProjectCard";
+import AboutSection from "./components/AboutSection";
+import ContactForm from "./components/ContactForm";
+import "./App.css";
 export default function App() {
   return (<>
     <Header />
     <About />
+    <AboutSection />
     <ProjectCard />
+    <ContactForm />
+    <Projects />
     <Contact />
+    <NotFound />
     <About />
     <Footer />
   </>
   );
 }
+export { Contact, About, Projects, Header, Footer, ProjectCard, NotFound };
